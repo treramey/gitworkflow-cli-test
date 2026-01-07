442b715a812c88c64a7abbbe1cf016854081709f
Trevor Ramey <trevor.ramey@silvervinesoftware.com>
2026-01-07 13:45:13 -0600

    feat: test full journey to master

--- apps/web/src/lib/utils.ts
+++ apps/web/src/lib/utils.ts
@@ -4,3 +4,4 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+// Test feature 1 - full journey Wed Jan  7 13:45:13 CST 2026
