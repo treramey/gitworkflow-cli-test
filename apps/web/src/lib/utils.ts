1ad15c098ce429fe9824f00ff5b0201fc10cbeec
Trevor Ramey <trevor.ramey@silvervinesoftware.com>
2026-01-07 13:45:15 -0600

    feat: test staging removal on PR close

--- apps/web/src/lib/utils.ts
+++ apps/web/src/lib/utils.ts
@@ -4,3 +4,4 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+// Test feature 2 - staging removal Wed Jan  7 13:45:15 CST 2026
